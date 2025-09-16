e3bec275f9ca4bcee64c45b891d6642960e19df7
Runar <s1208862@student.windesheim.nl>
2025-09-16 17:28:39 +0200

    Method GetAvailableProducts gemaakt De GetAvailableProducts method is nu klaar, en laat bij elke boodschappenlijst de juiste beschikbare producten zien.

--- Grocery.App/ViewModels/GroceryListItemsViewModel.cs
+++ Grocery.App/ViewModels/GroceryListItemsViewModel.cs
@@ -34,10 +34,14 @@ namespace Grocery.App.ViewModels
 
         private void GetAvailableProducts()
         {
-            //Maak de lijst AvailableProducts leeg
-            //Haal de lijst met producten op
-            //Controleer of het product al op de boodschappenlijst staat, zo niet zet het in de AvailableProducts lijst
-            //Houdt rekening met de voorraad (als die nul is kun je het niet meer aanbieden).            
+            AvailableProducts.Clear();
+            foreach (Product product in _productService.GetAll())
+            {
+                if (MyGroceryListItems.FirstOrDefault(x => x.ProductId == product.Id) == null && product.Stock > 0)
+                {
+                    AvailableProducts.Add(product);
+                }
+            }
         }
 
         partial void OnGroceryListChanged(GroceryList value)
